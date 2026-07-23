a1c49db1d0dc687c7b7331f76cc131191a440f43
AJAY2405 <rahul372602@gmail.com>
2026-07-23 16:26:59 +0530

    update 23/7/2026(vii)

--- backend/emailVerify/verifyMail.js
+++ backend/emailVerify/verifyMail.js
@@ -71,6 +71,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
